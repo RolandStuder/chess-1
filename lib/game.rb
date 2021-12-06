af43ecc8b32aed6de2dd87a7b2e1d04a124a4f00
DigitalKnight <asadredset@gmail.com>
2021-12-06 19:58:33 +0500

    Made initial stuff and files

---
+++ lib/game.rb
@@ -0,0 +1,28 @@
+class Game
+    def initialize
+    end
+
+    def play
+        welcome
+        if new_game? 
+            create_board
+            populate_board
+            create_players
+        else
+            retore_game
+        end
+
+        display_board
+        get_input
+        update_board
+        if checkmate?
+            decide_winner
+            announce_winner
+            break
+        elsif draw? 
+          announce_draw
+          break
+        end 
+        update_id 
+    end
+end
